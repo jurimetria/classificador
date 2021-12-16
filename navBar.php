e4635dd3882d9d1cd94020ea80ff940986df6c8f
juri <jurimetria@lp.com.br>
2021-12-16 15:05:03 -0300

    png na navbar

--- navBar.php
+++ navBar.php
@@ -2,6 +2,12 @@
     <div>
 
         <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+
+        <div>
+            <img src='https://lp-classificador.s3.amazonaws.com/img/LogoBranco+-+P1.png' alt='logo' width='80' height='80'>
+        
+        </div>
+
             <div class='container-fluid'>
                 <a class='navbar-brand' href='index.php'>L&P | Classificador de Pastas</a>
                 <button class='navbar-toggler ' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
